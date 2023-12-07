ecb9e660fe4b9386fa43edf47530a154994ccf14
Michał Błaszczyk <michal.blaszczyk00@gmail.com>
2023-12-07 14:55:58 +0100

    feat: new src folder

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,21 @@
+const buttonStart = document.querySelector('button.button-start');
+const buttonStop = document.querySelector('button.button-stop');
+
+buttonStart.addEventListener('click', () => {
+  buttonStart.setAttribute('disabled', true);
+  buttonStop.removeAttribute('disabled');
+ timerId = setInterval(() => {
+  const randomColor = getRandomHexColor();
+document.body.style.backgroundColor = randomColor;
+}, 1000);
+});
+
+buttonStop.addEventListener("click", () => {
+  buttonStart.removeAttribute('disabled');
+  buttonStop.setAttribute('disabled', true);
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
