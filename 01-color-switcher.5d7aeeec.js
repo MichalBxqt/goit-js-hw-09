124d46c2508d63e14c04be3c179788bfd29fd2e8
MichalBxqt <michal.blaszczyk00@gmail.com>
2023-12-07 13:56:34 +0000

    Deploying to gh-pages from @ MichalBxqt/goit-js-hw-09@ecb9e660fe4b9386fa43edf47530a154994ccf14 🚀

---
+++ 01-color-switcher.5d7aeeec.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button.button-start"),e=document.querySelector("button.button-stop");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),timerId=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled",!0),clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.5d7aeeec.js.map
